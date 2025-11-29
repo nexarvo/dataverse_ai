e4ba444fa53fcc0918daf9d57d30098435153729
usman-ghani564 <usmanghani564.ug9@gmail.com>
2025-11-30 03:39:22 +0500

    fix: sitemap visible

--- frontend/next-sitemap.config.js
+++ frontend/next-sitemap.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteUrl:
     process.env.NEXT_PUBLIC_SITE_URL || "https://dataverse-ai-smoky.vercel.app",
-  generateRobotsTxt: false, // we already added our own robots.txt
+  generateRobotsTxt: true,
   changefreq: "daily",
   priority: 0.7,
   sitemapSize: 7000,
